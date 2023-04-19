3f6e7818f9e376f686b4851a459964dff2c7fc45
Davian Pabon <124885691+altair201@users.noreply.github.com>
2023-04-19 14:46:20 -0500

    imc

---
+++ main.js
@@ -0,0 +1,11 @@
+const pesoInicial=document.getElementById("peso")
+const alturaInicial=document.getElementsByTagName("alt")
+const info=document.getElementById("info")
+function calcular(){
+    let pes=+pesoInicial.value;
+    let alt=+alturaInicial.value;
+    let imc=0
+    if(alt<=3 || alt>0){
+
+    }
+}
